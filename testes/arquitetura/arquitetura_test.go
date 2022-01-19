4c3cd7498f9dae49a6ea04ac8d57ace7601d1b03
Vinicius Tirabassi <vinicius@Viniciuss-MacBook-Air.local>
2022-01-18 22:06:18 -0300

    [feat] - add unitTest

--- testes/arquitetura/arquitetura_test.go
+++ testes/arquitetura/arquitetura_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestDependente(t *testing.T) {
-	t.Parallel()
+	t.Parallel()                   //mostra que esse teste pode ser executado de forma paralela com outros testes
 	if runtime.GOARCH == "amd64" { //filtro o test para executar apenas se a arquitetura for amd64
 		t.Skip("Não funciona em arquitetura amd64")
 	}
